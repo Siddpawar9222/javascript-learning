4a50d05b63d2d7903b8ea32adf98d30896bf1e87
Siddhesh <siddpawar583@gmail.com>
2024-10-22 21:31:43 +0530

    revision

--- 6_Array/reduce.js
+++ 6_Array/reduce.js
@@ -4,12 +4,9 @@
 // } ,45 )       // here 45 is already stored number in accumulator(Initial value of accumulator)
 // console.log(numUp);
 
-
-
 //Practice 
 var arr = [ 45 , 45 , 90 ];
 const k = arr.reduce((accumulator,Elements ,index ,Arrays)=>{
     return (accumulator +=Elements) ;
 }, 100) ;
-
 console.log(k);
